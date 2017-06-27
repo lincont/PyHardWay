54b26b3753362c9642bcfd5194b7a445bf079f33
lincont <tania.lincoln@gmail.com>
2017-06-26 23:50:27 -0500

    Initial commit for Python the Hard Way Excercises

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# Learning print statements
+
+print "Hello World"
+print "Hello Again"
+print "I like typing this"
+print "This is fun."
+print 'Yay, printing!'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this'
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'TML TML'
+my_age = 35
+my_height = 64 #inches
+my_weight = 120 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White-ish'
+my_hair = "Brown"
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "He's %d pounds heavy" % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee" % my_teeth
+
+# this line is tricky, try to get it right
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
